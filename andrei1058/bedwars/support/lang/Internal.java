5a547837d5ed7c66f7bc301b9e8608f5438455c4
andrei1058 <andrew.dascalu@gmail.com>
2018-08-28 21:26:12 +0200

    added PlayerLangChange

--- andrei1058/bedwars/support/lang/Internal.java
+++ andrei1058/bedwars/support/lang/Internal.java
@@ -1,10 +1,12 @@
 package com.andrei1058.bedwars.support.lang;
 
+import com.andrei1058.bedwars.api.PlayerLangChangeEvent;
 import com.andrei1058.bedwars.arena.Arena;
 import com.andrei1058.bedwars.arena.Misc;
 import com.andrei1058.bedwars.arena.SBoard;
 import com.andrei1058.bedwars.configuration.ConfigManager;
 import com.andrei1058.bedwars.configuration.Language;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
@@ -28,11 +30,18 @@ public class Internal implements Lang {
 
     @Override
     public void setLang(Player p, String iso) {
+        Language newLang = Language.getLang(iso);
+        Language oldLang = Language.getLangByPlayer().containsKey(p) ? Language.getPlayerLanguage(p) : Language.getLanguages().get(0);
+        PlayerLangChangeEvent e = new PlayerLangChangeEvent(p, oldLang, newLang);
+        Bukkit.getPluginManager().callEvent(e);
+        if (e.isCancelled()) return;
+
         if (Language.getLangByPlayer().containsKey(p)){
-            Language.getLangByPlayer().replace(p, Language.getLang(iso));
+            Language.getLangByPlayer().replace(p, lang);
         } else {
-            Language.getLangByPlayer().put(p, Language.getLang(iso));
+            Language.getLangByPlayer().put(p, lang);
         }
+
         x.set(p.getName(), iso);
         if (config.getLobbyWorldName().equalsIgnoreCase(p.getWorld().getName())){
             Arena.sendMultiarenaLobbyItems(p);
