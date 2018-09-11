ffbbb154362dbe97f45aaf351698603e52b16635
andrei1058 <andrew.dascalu@gmail.com>
2018-09-11 14:17:59 +0200

    bw leave command improvements

--- andrei1058/bedwars/commands/LeaveCommand.java
+++ andrei1058/bedwars/commands/LeaveCommand.java
@@ -1,20 +1,10 @@
 package com.andrei1058.bedwars.commands;
 
-import com.andrei1058.bedwars.api.ServerType;
-import com.andrei1058.bedwars.arena.Arena;
-import com.andrei1058.bedwars.arena.Misc;
-import com.andrei1058.bedwars.configuration.Language;
-import com.andrei1058.bedwars.configuration.Messages;
-import com.google.common.io.ByteArrayDataOutput;
-import com.google.common.io.ByteStreams;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.entity.Player;
-
-import static com.andrei1058.bedwars.Main.*;
-import static com.andrei1058.bedwars.configuration.Language.getMsg;
 
 public class LeaveCommand extends BukkitCommand {
 
@@ -26,20 +16,7 @@ public class LeaveCommand extends BukkitCommand {
     public boolean execute(CommandSender s, String st, String[] args) {
         if (s instanceof ConsoleCommandSender) return true;
         Player p = (Player) s;
-        Arena a = Arena.getArenaByPlayer(p);
-        if (a == null){
-            if (getServerType() == ServerType.MULTIARENA && spigot.getBoolean("settings.bungeecord")){
-                Misc.moveToLobbyOrKick(p);
-            } else {
-                p.sendMessage(getMsg(p, Messages.COMMAND_LEAVE_DENIED_NOT_IN_ARENA));
-            }
-        } else {
-            if (a.isPlayer(p)){
-                a.removePlayer(p, false);
-            } else if (a.isSpectator(p)){
-                a.removeSpectator(p, false);
-            }
-        }
+        Bukkit.dispatchCommand(p, "bw leave");
         return true;
     }
 }
