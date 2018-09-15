e5579f7141be0f078349f6abadb0608d482a9e5f
andrei1058 <andrew.dascalu@gmail.com>
2018-09-15 17:06:58 +0200

    code clean-up

--- andrei1058/bedwars/listeners/EntityDropPickListener.java
+++ andrei1058/bedwars/listeners/EntityDropPickListener.java
@@ -14,12 +14,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
-import static com.andrei1058.bedwars.Main.config;
-import static com.andrei1058.bedwars.Main.nms;
 
 public class EntityDropPickListener implements Listener {
 
