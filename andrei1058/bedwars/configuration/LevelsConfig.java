8b82723df894153ffb5c463a5c77cbf56c72cd3c
andrei1058 <andrew.dascalu@gmail.com>
2019-03-26 22:04:38 +0100

    added space in level names

--- andrei1058/bedwars/configuration/LevelsConfig.java
+++ andrei1058/bedwars/configuration/LevelsConfig.java
@@ -18,19 +18,19 @@ public class LevelsConfig extends ConfigManager {
         levels.getYml().options().copyDefaults(true);
         if (levels.isFirstTime()){
 
-            levels.getYml().addDefault("levels.1.name", "&a{number} ✩");
+            levels.getYml().addDefault("levels.1.name", "&a{number} ✩ ");
             levels.getYml().addDefault("levels.1.rankup-cost", 1000);
 
-            levels.getYml().addDefault("levels.2.name", "&a{number} ✩");
+            levels.getYml().addDefault("levels.2.name", "&a{number} ✩ ");
             levels.getYml().addDefault("levels.2.rankup-cost", 2000);
 
-            levels.getYml().addDefault("levels.3.name", "&a{number} ✩");
+            levels.getYml().addDefault("levels.3.name", "&a{number} ✩ ");
             levels.getYml().addDefault("levels.3.rankup-cost", 3000);
 
-            levels.getYml().addDefault("levels.4.name", "&a{number} ✩");
+            levels.getYml().addDefault("levels.4.name", "&a{number} ✩ ");
             levels.getYml().addDefault("levels.4.rankup-cost", 3500);
 
-            levels.getYml().addDefault("levels.others.name", "&d{number} ✩");
+            levels.getYml().addDefault("levels.others.name", "&d{number} ✩ ");
             levels.getYml().addDefault("levels.others.rankup-cost", 5000);
         }
 
