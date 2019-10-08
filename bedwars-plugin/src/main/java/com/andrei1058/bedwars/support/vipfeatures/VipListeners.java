c0387faa013bc23f943135a080d13a671b2882fc
andrei1058 <andrew.dascalu@gmail.com>
2019-10-08 16:47:46 +0200

    fixed issue where you can break beds with VipFeatures Spells

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/support/vipfeatures/VipListeners.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/support/vipfeatures/VipListeners.java
@@ -1,9 +1,12 @@
 package com.andrei1058.bedwars.support.vipfeatures;
 
 import com.andrei1058.bedwars.BedWars;
+import com.andrei1058.bedwars.api.arena.IArena;
+import com.andrei1058.bedwars.api.arena.team.ITeam;
 import com.andrei1058.bedwars.api.events.player.PlayerJoinArenaEvent;
 import com.andrei1058.bedwars.api.server.ServerType;
 import com.andrei1058.vipfeatures.api.IVipFeatures;
+import com.andrei1058.vipfeatures.api.event.BlockChangeEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,7 +16,7 @@ public class VipListeners implements Listener {
 
     private IVipFeatures api;
 
-    public VipListeners(IVipFeatures api){
+    public VipListeners(IVipFeatures api) {
         this.api = api;
     }
 
@@ -31,5 +34,24 @@ public class VipListeners implements Listener {
         Bukkit.getScheduler().runTaskLater(BedWars.plugin, () -> {
             api.givePlayerItemStack(e.getPlayer());
         }, 10L);
+    }
+
+    @EventHandler
+    public void onBockChange(BlockChangeEvent e) {
+        if (BedWars.getAPI().getArenaUtil().getArenaByName(e.getLocation().getWorld().getName()) != null) {
+            IArena a = BedWars.getAPI().getArenaUtil().getArenaByName(e.getLocation().getWorld().getName());
+            for (ITeam t : a.getTeams()) {
+                for (int x = -1; x < 2; x++) {
+                    for (int z = -1; z < 2; z++) {
+                        if (e.getLocation().getBlockX() == t.getBed().getBlockX() &&
+                                e.getLocation().getBlockY() == t.getBed().getBlockY() &&
+                                e.getLocation().getBlockZ() == t.getBed().getBlockZ()) {
+                            if (BedWars.nms.isBed(t.getBed().clone().add(x, 0, z).getBlock().getType())) e.setCancelled(true);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
