059e51fc29f787272c38f629e532765188d09149
andrei1058 <andrew.dascalu@gmail.com>
2019-11-11 16:09:22 +0100

    prevent players form opening doors

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/arena/spectator/SpectatorListeners.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/arena/spectator/SpectatorListeners.java
@@ -52,9 +52,9 @@ public class SpectatorListeners implements Listener {
 
     @EventHandler
     public void onSpectatorBlockInteract(PlayerInteractEvent e) {
-        if (e.getAction() != Action.PHYSICAL) return;
+        if (e.getClickedBlock() == null) return;
         if (!BedWars.getAPI().getArenaUtil().isSpectating(e.getPlayer())) return;
-
+        if (e.getClickedBlock().getType().toString().contains("DOOR"))
         // Disable spectator interact
         e.setCancelled(true);
     }
