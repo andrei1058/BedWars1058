4a6290b27dbb3f081cf595a5b3b132667542cfea
andrei1058 <andrew.dascalu@gmail.com>
2019-11-16 20:44:40 +0100

    fixed issue where you can't open the shop after rejoin

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/shop/listeners/QuickBuyListener.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/shop/listeners/QuickBuyListener.java
@@ -2,6 +2,7 @@ package com.andrei1058.bedwars.shop.listeners;
 
 import com.andrei1058.bedwars.api.arena.IArena;
 import com.andrei1058.bedwars.api.events.player.PlayerJoinArenaEvent;
+import com.andrei1058.bedwars.api.events.player.PlayerReJoinEvent;
 import com.andrei1058.bedwars.arena.Arena;
 import com.andrei1058.bedwars.shop.quickbuy.PlayerQuickBuyCache;
 import org.bukkit.event.EventHandler;
@@ -14,6 +15,15 @@ public class QuickBuyListener implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void onArenaJoin(PlayerJoinArenaEvent e){
         if (e.isSpectator()) return;
+        PlayerQuickBuyCache cache = PlayerQuickBuyCache.getQuickBuyCache(e.getPlayer().getUniqueId());
+        if (cache != null) {
+            cache.destroy();
+        }
+        new PlayerQuickBuyCache(e.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onArenaJoin(PlayerReJoinEvent e){
         PlayerQuickBuyCache cache = PlayerQuickBuyCache.getQuickBuyCache(e.getPlayer().getUniqueId());
         if (cache != null) {
             cache.destroy();
