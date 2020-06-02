eec0c15669aea917621f46593837d96a0aaac236
andrei1058 <andrew.dascalu@gmail.com>
2020-06-02 13:09:41 +0200

    trying to fix crash issue

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/DamageDeathMove.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/DamageDeathMove.java
@@ -376,7 +376,7 @@ public class DamageDeathMove implements Listener {
 
             /* call game kill event */
             Bukkit.getPluginManager().callEvent(new PlayerKillEvent(a, victim, killer, message, cause));
-            Bukkit.getScheduler().runTaskLater(plugin, () -> victim.spigot().respawn(), 1L);
+            Bukkit.getScheduler().runTaskLater(plugin, () -> victim.spigot().respawn(), 3L);
             a.addPlayerDeath(victim);
         }
     }
