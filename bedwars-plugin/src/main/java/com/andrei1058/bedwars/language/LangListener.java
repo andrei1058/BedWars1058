f92f4f46eaf80912e51541d0717fc5cb624ae7e4
andrei1058 <andrew.dascalu@gmail.com>
2020-10-14 17:56:54 +0200

    bw lang fixed

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/language/LangListener.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/language/LangListener.java
@@ -5,13 +5,13 @@ import com.andrei1058.bedwars.api.events.player.PlayerLangChangeEvent;
 import com.andrei1058.bedwars.arena.Arena;
 import com.andrei1058.bedwars.sidebar.BedWarsScoreboard;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 public class LangListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onLanguageChangeEvent(PlayerLangChangeEvent e) {
         if (e == null) return;
         if (e.isCancelled()) return;
@@ -19,6 +19,9 @@ public class LangListener implements Listener {
             Bukkit.getScheduler().runTaskLater(BedWars.plugin, () -> {
                 Arena.sendLobbyCommandItems(e.getPlayer());
                 BedWarsScoreboard.giveScoreboard(e.getPlayer(), Arena.getArenaByPlayer(e.getPlayer()), false);
+
+                // save to db
+                Bukkit.getScheduler().runTaskAsynchronously(BedWars.plugin, ()-> BedWars.getRemoteDatabase().setLanguage(e.getPlayer().getUniqueId(), e.getNewLang()));
             }, 10L);
         }
     }
