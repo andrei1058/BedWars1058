f884834422a3993b65e832a5409f55ad64083332
andrei1058 <andrew.dascalu@gmail.com>
2021-01-24 20:53:00 +0100

    fixed issue where you get items as spectator from previous kill

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/dropshandler/PlayerDrops.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/dropshandler/PlayerDrops.java
@@ -83,50 +83,50 @@ public class PlayerDrops {
 
             } else {
                 // add-to-inventory feature if receiver is not respawning
-                if (!arena.isReSpawning(killer)) {
-                    Map<Material, Integer> materialDrops = new HashMap<>();
-                    for (ItemStack i : inventory) {
-                        if (i == null) continue;
-                        if (i.getType() == Material.AIR) continue;
-                        if (i.getType() == Material.DIAMOND || i.getType() == Material.EMERALD || i.getType() == Material.IRON_INGOT || i.getType() == Material.GOLD_INGOT) {
+                if (!arena.isPlayer(killer)) return true;
+                if (arena.isReSpawning(killer)) return true;
+                Map<Material, Integer> materialDrops = new HashMap<>();
+                for (ItemStack i : inventory) {
+                    if (i == null) continue;
+                    if (i.getType() == Material.AIR) continue;
+                    if (i.getType() == Material.DIAMOND || i.getType() == Material.EMERALD || i.getType() == Material.IRON_INGOT || i.getType() == Material.GOLD_INGOT) {
 
-                            // add to killer inventory
-                            killer.getInventory().addItem(i);
+                        // add to killer inventory
+                        killer.getInventory().addItem(i);
 
-                            // count items
-                            if (materialDrops.containsKey(i.getType())) {
-                                materialDrops.replace(i.getType(), materialDrops.get(i.getType()) + i.getAmount());
-                            } else {
-                                materialDrops.put(i.getType(), i.getAmount());
-                            }
+                        // count items
+                        if (materialDrops.containsKey(i.getType())) {
+                            materialDrops.replace(i.getType(), materialDrops.get(i.getType()) + i.getAmount());
+                        } else {
+                            materialDrops.put(i.getType(), i.getAmount());
                         }
                     }
-
-                    for (Map.Entry<Material, Integer> entry : materialDrops.entrySet()) {
-                        String msg = "";
-                        int amount = entry.getValue();
-                        switch (entry.getKey()) {
-                            case DIAMOND:
-                                msg = getMsg(killer, Messages.PLAYER_DIE_REWARD_DIAMOND).replace("{meaning}", amount == 1 ?
-                                        getMsg(killer, Messages.MEANING_DIAMOND_SINGULAR) : getMsg(killer, Messages.MEANING_DIAMOND_PLURAL));
-                                break;
-                            case EMERALD:
-                                msg = getMsg(killer, Messages.PLAYER_DIE_REWARD_EMERALD).replace("{meaning}", amount == 1 ?
-                                        getMsg(killer, Messages.MEANING_EMERALD_SINGULAR) : getMsg(killer, Messages.MEANING_EMERALD_PLURAL));
-                                break;
-                            case IRON_INGOT:
-                                msg = getMsg(killer, Messages.PLAYER_DIE_REWARD_IRON).replace("{meaning}", amount == 1 ?
-                                        getMsg(killer, Messages.MEANING_IRON_SINGULAR) : getMsg(killer, Messages.MEANING_IRON_PLURAL));
-                                break;
-                            case GOLD_INGOT:
-                                msg = getMsg(killer, Messages.PLAYER_DIE_REWARD_GOLD).replace("{meaning}", amount == 1 ?
-                                        getMsg(killer, Messages.MEANING_GOLD_SINGULAR) : getMsg(killer, Messages.MEANING_GOLD_PLURAL));
-                                break;
-                        }
-                        killer.sendMessage(msg.replace("{amount}", String.valueOf(amount)));
-                    }
-                    materialDrops.clear();
                 }
+
+                for (Map.Entry<Material, Integer> entry : materialDrops.entrySet()) {
+                    String msg = "";
+                    int amount = entry.getValue();
+                    switch (entry.getKey()) {
+                        case DIAMOND:
+                            msg = getMsg(killer, Messages.PLAYER_DIE_REWARD_DIAMOND).replace("{meaning}", amount == 1 ?
+                                    getMsg(killer, Messages.MEANING_DIAMOND_SINGULAR) : getMsg(killer, Messages.MEANING_DIAMOND_PLURAL));
+                            break;
+                        case EMERALD:
+                            msg = getMsg(killer, Messages.PLAYER_DIE_REWARD_EMERALD).replace("{meaning}", amount == 1 ?
+                                    getMsg(killer, Messages.MEANING_EMERALD_SINGULAR) : getMsg(killer, Messages.MEANING_EMERALD_PLURAL));
+                            break;
+                        case IRON_INGOT:
+                            msg = getMsg(killer, Messages.PLAYER_DIE_REWARD_IRON).replace("{meaning}", amount == 1 ?
+                                    getMsg(killer, Messages.MEANING_IRON_SINGULAR) : getMsg(killer, Messages.MEANING_IRON_PLURAL));
+                            break;
+                        case GOLD_INGOT:
+                            msg = getMsg(killer, Messages.PLAYER_DIE_REWARD_GOLD).replace("{meaning}", amount == 1 ?
+                                    getMsg(killer, Messages.MEANING_GOLD_SINGULAR) : getMsg(killer, Messages.MEANING_GOLD_PLURAL));
+                            break;
+                    }
+                    killer.sendMessage(msg.replace("{amount}", String.valueOf(amount)));
+                }
+                materialDrops.clear();
             }
 
         }
