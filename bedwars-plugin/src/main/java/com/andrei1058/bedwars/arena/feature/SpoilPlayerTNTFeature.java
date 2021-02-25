ea27a34c33de825131cfc7a29d4c4168071c5899
andrei1058 <andrew.dascalu@gmail.com>
2021-02-25 12:27:47 +0100

    fixed issue where tnt particle remains at lobby

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/arena/feature/SpoilPlayerTNTFeature.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/arena/feature/SpoilPlayerTNTFeature.java
@@ -3,6 +3,7 @@ package com.andrei1058.bedwars.arena.feature;
 import com.andrei1058.bedwars.BedWars;
 import com.andrei1058.bedwars.api.arena.IArena;
 import com.andrei1058.bedwars.api.configuration.ConfigPath;
+import com.andrei1058.bedwars.api.events.player.PlayerLeaveArenaEvent;
 import com.andrei1058.bedwars.api.events.shop.ShopBuyEvent;
 import com.andrei1058.bedwars.arena.Arena;
 import org.bukkit.Bukkit;
@@ -48,6 +49,11 @@ public class SpoilPlayerTNTFeature {
     }
 
     private static class TNTListener implements Listener {
+
+        @EventHandler
+        public void onLeave(PlayerLeaveArenaEvent event){
+            instance.playersWithTnt.remove(event.getPlayer());
+        }
 
         @EventHandler(ignoreCancelled = true)
         public void onPickUp(PlayerPickupItemEvent event) {
