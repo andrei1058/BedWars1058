cccb01693287e30e4ce2b9155835585711d331f9
andrei1058 <andrew.dascalu@gmail.com>
2021-02-25 12:53:47 +0100

    player health on head improvements & re-added javadocs deployment

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/sidebar/ScoreboardListener.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/sidebar/ScoreboardListener.java
@@ -19,18 +19,18 @@ import org.bukkit.event.entity.EntityRegainHealthEvent;
 public class ScoreboardListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onPlayerDamage(EntityDamageEvent e){
+    public void onPlayerDamage(EntityDamageEvent e) {
         if (e == null) return;
         if (e.isCancelled()) return;
         if (!(e.getEntity() instanceof Player)) return;
         final Player player = (Player) e.getEntity();
         final IArena arena = Arena.getArenaByPlayer(player);
 
-        int health = (int) (player.getHealth() - e.getDamage()) + 1;
+        int health = (int) Math.ceil((player.getHealth() - e.getFinalDamage()));
 
         if (arena == null) return;
 
-        for (BedWarsScoreboard scoreboard : BedWarsScoreboard.getScoreboards().values()){
+        for (BedWarsScoreboard scoreboard : BedWarsScoreboard.getScoreboards().values()) {
             if (arena.equals(scoreboard.getArena())) {
                 scoreboard.getHandle().refreshHealth(player, health);
             }
@@ -38,7 +38,7 @@ public class ScoreboardListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onRegain(EntityRegainHealthEvent e){
+    public void onRegain(EntityRegainHealthEvent e) {
         if (e == null) return;
         if (e.isCancelled()) return;
         if (!(e.getEntity() instanceof Player)) return;
@@ -46,30 +46,28 @@ public class ScoreboardListener implements Listener {
         final IArena arena = Arena.getArenaByPlayer(player);
         if (arena == null) return;
 
-        int health = (int) (player.getHealth() + e.getAmount()) + 1;
-        if (health > player.getMaxHealth()){
-            health -= 1;
-        }
-        for (BedWarsScoreboard scoreboard : BedWarsScoreboard.getScoreboards().values()){
-            if (arena.equals(scoreboard.getArena())){
+        int health = (int) Math.ceil(player.getHealth() + e.getAmount());
+
+        for (BedWarsScoreboard scoreboard : BedWarsScoreboard.getScoreboards().values()) {
+            if (arena.equals(scoreboard.getArena())) {
                 scoreboard.getHandle().refreshHealth(player, health);
             }
         }
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onReSpawn(PlayerReSpawnEvent e){
+    public void onReSpawn(PlayerReSpawnEvent e) {
         if (e == null) return;
         final IArena arena = e.getArena();
-        for (BedWarsScoreboard scoreboard : BedWarsScoreboard.getScoreboards().values()){
-            if (arena.equals(scoreboard.getArena())){
-                scoreboard.getHandle().refreshHealth(e.getPlayer(), (int) e.getPlayer().getHealth());
+        for (BedWarsScoreboard scoreboard : BedWarsScoreboard.getScoreboards().values()) {
+            if (arena.equals(scoreboard.getArena())) {
+                scoreboard.getHandle().refreshHealth(e.getPlayer(), (int) Math.ceil(e.getPlayer().getHealth()));
             }
         }
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
-    public void reJoin(PlayerReJoinEvent e){
+    public void reJoin(PlayerReJoinEvent e) {
         if (e == null) return;
         if (e.isCancelled()) return;
         if (!BedWars.config.getBoolean(ConfigPath.SB_CONFIG_SIDEBAR_LIST_FORMAT_PLAYING)) return;
@@ -77,35 +75,35 @@ public class ScoreboardListener implements Listener {
         final Player player = e.getPlayer();
 
         // re-add player to scoreboard tab list
-        for (BedWarsScoreboard scoreboard : BedWarsScoreboard.getScoreboards().values()){
-            if (arena.equals(scoreboard.getArena())){
+        for (BedWarsScoreboard scoreboard : BedWarsScoreboard.getScoreboards().values()) {
+            if (arena.equals(scoreboard.getArena())) {
                 scoreboard.addToTabList(player, Messages.FORMATTING_SCOREBOARD_TAB_PREFIX_PLAYING, Messages.FORMATTING_SCOREBOARD_TAB_SUFFIX_PLAYING);
             }
         }
     }
 
     @EventHandler
-    public void onBedDestroy(PlayerBedBreakEvent e){
+    public void onBedDestroy(PlayerBedBreakEvent e) {
         if (e == null) return;
         final IArena arena = e.getArena();
 
         // refresh placeholders in case placeholders refresh is disabled
         BedWarsScoreboard.getScoreboards().values().forEach(bedWarsScoreboard -> {
-            if (arena.equals(bedWarsScoreboard.getArena())){
+            if (arena.equals(bedWarsScoreboard.getArena())) {
                 bedWarsScoreboard.getHandle().refreshPlaceholders();
             }
         });
     }
 
     @EventHandler
-    public void onFinalKill(PlayerKillEvent e){
+    public void onFinalKill(PlayerKillEvent e) {
         if (e == null) return;
         if (!e.getCause().isFinalKill()) return;
         final IArena arena = e.getArena();
 
         // refresh placeholders in case placeholders refresh is disabled
         BedWarsScoreboard.getScoreboards().values().forEach(bedWarsScoreboard -> {
-            if (arena.equals(bedWarsScoreboard.getArena())){
+            if (arena.equals(bedWarsScoreboard.getArena())) {
                 bedWarsScoreboard.getHandle().refreshPlaceholders();
             }
         });
