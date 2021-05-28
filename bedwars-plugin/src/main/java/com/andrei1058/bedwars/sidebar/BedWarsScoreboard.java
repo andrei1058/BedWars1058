467911f5347ac22c674c81582cf0822aa3497c0c
andrei1058 <andrew.dascalu@gmail.com>
2021-05-29 05:27:29 +0930

    fixed #729

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/sidebar/BedWarsScoreboard.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/sidebar/BedWarsScoreboard.java
@@ -55,7 +55,7 @@ public class BedWarsScoreboard {
         }
 
         // Cache the next event date format
-        nextEventDateFormat = new SimpleDateFormat(getMsg(player, Messages.FORMATTING_SCOREBOARD_NEXEVENT_TIMER));
+        nextEventDateFormat = new SimpleDateFormat(getMsg(player, Messages.FORMATTING_SCOREBOARD_NEXEVENT_TIMER), Locale.ITALY);
         dateFormat = new SimpleDateFormat(getMsg(player, Messages.FORMATTING_SCOREBOARD_DATE));
 
         // Define common placeholders
