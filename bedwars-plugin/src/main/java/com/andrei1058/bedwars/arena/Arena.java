be7a47b65fd2b7e4c234b702cef41d567583a9b9
ajgeiss0702 <ajgeiss72@gmail.com>
2021-12-06 10:39:15 -0700

    add {vPrefix} to join message

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/arena/Arena.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/arena/Arena.java
@@ -498,7 +498,7 @@ public class Arena implements IArena {
             p.setAllowFlight(false);
             p.setHealth(20);
             for (Player on : players) {
-                on.sendMessage(getMsg(on, Messages.COMMAND_JOIN_PLAYER_JOIN_MSG).replace("{playername}", p.getName()).replace("{player}", p.getDisplayName()).replace("{on}", String.valueOf(getPlayers().size())).replace("{max}", String.valueOf(getMaxPlayers())));
+                on.sendMessage(getMsg(on, Messages.COMMAND_JOIN_PLAYER_JOIN_MSG).replace("{vPrefix}", getChatSupport().getPrefix(p)).replace("{playername}", p.getName()).replace("{player}", p.getDisplayName()).replace("{on}", String.valueOf(getPlayers().size())).replace("{max}", String.valueOf(getMaxPlayers())));
             }
             setArenaByPlayer(p, this);
 
