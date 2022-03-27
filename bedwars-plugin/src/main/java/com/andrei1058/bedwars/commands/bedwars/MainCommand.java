875c2d31a0ec1ca108d3f2cb3b856d43038eb45a
Ricardo <63428864+reussy@users.noreply.github.com>
2022-03-27 14:57:54 -0600

    Implement command for set max build Y (#428)

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/commands/bedwars/MainCommand.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/commands/bedwars/MainCommand.java
@@ -97,6 +97,7 @@ public class MainCommand extends BukkitCommand implements ParentCommand {
         new com.andrei1058.bedwars.commands.bedwars.subcmds.sensitive.setup.WaitingPos(this, "waitingPos");
         new com.andrei1058.bedwars.commands.bedwars.subcmds.sensitive.setup.RemoveTeam(this, "removeTeam");
         new com.andrei1058.bedwars.commands.bedwars.subcmds.sensitive.setup.SetMaxInTeam(this, "setMaxInTeam");
+        new com.andrei1058.bedwars.commands.bedwars.subcmds.sensitive.setup.SetBuildHeight(this, "setMaxBuildHeight");
         new com.andrei1058.bedwars.commands.bedwars.subcmds.sensitive.setup.SetSpawn(this, "setSpawn");
         new com.andrei1058.bedwars.commands.bedwars.subcmds.sensitive.setup.SetBed(this, "setBed");
         new SetShop(this, "setShop");
