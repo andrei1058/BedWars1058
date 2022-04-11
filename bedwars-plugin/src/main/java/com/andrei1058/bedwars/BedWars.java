c033d806154a990bbbd6a940789cd065791b4ffb
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-04-11 18:18:09 -0400

    Made Healpool event register Async
    Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
                    if (config.getBoolean(ConfigPath.GENERAL_CONFIGURATION_HEAL_POOL))  //heal pool
                        registerEvents(new HealPoolListner());
            });

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/BedWars.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/BedWars.java
@@ -299,8 +299,10 @@ public class BedWars extends JavaPlugin {
         registerEvents(new EnderPearlLanded(), new QuitAndTeleportListener(), new BreakPlace(), new DamageDeathMove(), new Inventory(), new Interact(), new RefreshGUI(), new HungerWeatherSpawn(), new CmdProcess(),
                 new FireballListener(), new EggBridge(), new SpectatorListeners(), new BaseListener(), new TargetListener(), new LangListener(), new Warnings(this), new ChatAFK());
 
-        if (config.getBoolean(ConfigPath.GENERAL_CONFIGURATION_HEAL_POOL))  //heal pool
-            registerEvents(new HealPoolListner());
+        Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
+                if (config.getBoolean(ConfigPath.GENERAL_CONFIGURATION_HEAL_POOL))  //heal pool
+                    registerEvents(new HealPoolListner());
+        });
 
         if (getServerType() == ServerType.BUNGEE) {
             if (autoscale) {
