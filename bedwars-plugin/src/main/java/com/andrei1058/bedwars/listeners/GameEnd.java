3fc273383680043ef914306fbded3b9e3996cf5f
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-04-12 10:16:01 -0400

    Clear Winning teams inventory when game ends and prevent pick up items Stops Generator from dropping items

---
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/GameEnd.java
@@ -0,0 +1,30 @@
+package com.andrei1058.bedwars.listeners;
+
+import com.andrei1058.bedwars.api.arena.generator.IGenerator;
+import com.andrei1058.bedwars.api.events.gameplay.GameEndEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.UUID;
+
+public class GameEnd implements Listener {
+
+    @EventHandler
+    public void stopPickUP(GameEndEvent event){
+        if (!event.getAliveWinners().isEmpty()){
+            for (UUID p : event.getAliveWinners()) {
+                Bukkit.getPlayer(p).setCanPickupItems(false);
+                Bukkit.getPlayer(p).getInventory().clear();
+            }
+                for (IGenerator gen : event.getArena().getOreGenerators()) {
+                    ItemStack air = new ItemStack(Material.AIR, 1);
+                    gen.setOre(air);
+                    gen.setDelay(30);  //Stops generator from dropping items on game end
+                }
+        }
+    }
+
+}
