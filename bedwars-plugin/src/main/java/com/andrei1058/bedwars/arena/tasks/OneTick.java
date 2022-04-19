5bb01ad5e61294a9c40f17e17ce2077d55ad2f30
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-04-18 22:18:05 -0400

    Extra Null check to fix error

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/arena/tasks/OneTick.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/arena/tasks/OneTick.java
@@ -26,10 +26,10 @@ import com.andrei1058.bedwars.arena.OreGenerator;
 public class OneTick implements Runnable {
     @Override
     public void run() {
-
         //OneTick generators
         for (IGenerator h : OreGenerator.getRotation()) {
-            h.rotate();
+            if (h != null)
+                h.rotate();
         }
     }
 }
