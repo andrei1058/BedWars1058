20b0f910162f40e0c13d229a443efb12fa26af21
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-05-29 20:29:11 -0400

    Use Lambas

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/Warnings.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/Warnings.java
@@ -19,15 +19,11 @@ public class Warnings implements Listener {
         if(!player.isOp()) return;
 
         if (Bukkit.getPluginManager().isPluginEnabled("Multiverse-Core")) {
-            Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                player.sendMessage(ChatColor.RED + "[BedWars1058] Multiverse-Core detected! Please remove it or make sure it won't touch BedWars maps!");
-            }, 5); // run after 5 ticks to make sure its after any update spam on join
+            Bukkit.getScheduler().runTaskLater(plugin, () -> player.sendMessage(ChatColor.RED + "[BedWars1058] Multiverse-Core detected! Please remove it or make sure it won't touch BedWars maps!"), 5); // run after 5 ticks to make sure its after any update spam on join
         }
 
         if(Bukkit.getServer().getSpawnRadius() > 0) {
-            Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                player.sendMessage(ChatColor.RED + "[BedWars1058] Your spawn-protection in server.properties is enabled. "+ChatColor.YELLOW+"This might mess with BedWars arenas!"+ChatColor.GRAY+" I highly reccomend setting it to 0.");
-            }, 5);
+            Bukkit.getScheduler().runTaskLater(plugin, () -> player.sendMessage(ChatColor.RED + "[BedWars1058] Your spawn-protection in server.properties is enabled. "+ChatColor.YELLOW+"This might mess with BedWars arenas!"+ChatColor.GRAY+" I highly reccomend setting it to 0."), 5);
         }
     }
 }
--- versionsupport_v1_16_R3/src/main/java/com/andrei1058/bedwars/support/version/v1_16_R3/IGolem.java
+++ versionsupport_v1_16_R3/src/main/java/com/andrei1058/bedwars/support/version/v1_16_R3/IGolem.java
@@ -51,16 +51,10 @@ public class IGolem extends EntityIronGolem {
         this.targetSelector.a(1, new PathfinderGoalHurtByTarget(this));
         this.goalSelector.a(3, new PathfinderGoalRandomStroll(this, 1D));
         this.goalSelector.a(4, new PathfinderGoalRandomLookaround(this));
-        this.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget(this, EntityHuman.class, 20, true, false, player -> {
-            return ((EntityHuman)player).isAlive() && !team.wasMember(((EntityHuman)player).getUniqueID()) && !team.getArena().isReSpawning(((EntityHuman)player).getUniqueID())
-                    && !team.getArena().isSpectator(((EntityHuman)player).getUniqueID());
-        }));
-        this.targetSelector.a(3, new PathfinderGoalNearestAttackableTarget(this, IGolem.class, 20, true, false, golem -> {
-            return ((IGolem)golem).getTeam() != team;
-        }));
-        this.targetSelector.a(4, new PathfinderGoalNearestAttackableTarget(this, Silverfish.class, 20, true, false, sf -> {
-            return ((Silverfish)sf).getTeam() != team;
-        }));
+        this.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget(this, EntityHuman.class, 20, true, false, player -> ((EntityHuman)player).isAlive() && !team.wasMember(((EntityHuman)player).getUniqueID()) && !team.getArena().isReSpawning(((EntityHuman)player).getUniqueID())
+                && !team.getArena().isSpectator(((EntityHuman)player).getUniqueID())));
+        this.targetSelector.a(3, new PathfinderGoalNearestAttackableTarget(this, IGolem.class, 20, true, false, golem -> ((IGolem)golem).getTeam() != team));
+        this.targetSelector.a(4, new PathfinderGoalNearestAttackableTarget(this, Silverfish.class, 20, true, false, sf -> ((Silverfish)sf).getTeam() != team));
     }
 
     public ITeam getTeam() {
