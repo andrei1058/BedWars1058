0a9254c3980872bc00c7b19c8726328f883b381b
Xxyuri2005xX <yuri.carniti@gmail.com>
2022-08-18 21:48:37 +0200

    update the ITeam#getMembersCache to IArena#maxInTeam

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/money/internal/MoneyListeners.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/money/internal/MoneyListeners.java
@@ -1,6 +1,7 @@
 package com.andrei1058.bedwars.money.internal;
 
 import com.andrei1058.bedwars.BedWars;
+import com.andrei1058.bedwars.api.arena.IArena;
 import com.andrei1058.bedwars.api.arena.team.ITeam;
 import com.andrei1058.bedwars.api.events.gameplay.GameEndEvent;
 import com.andrei1058.bedwars.api.events.player.PlayerBedBreakEvent;
@@ -22,37 +23,37 @@ public class MoneyListeners implements Listener {
      */
     @EventHandler
     public void onGameEnd(GameEndEvent e) {
-        for (UUID p : e.getWinners ()) {
-            Player player = Bukkit.getPlayer ( p );
+        for (UUID p : e.getWinners()) {
+            Player player = Bukkit.getPlayer(p);
             if (player == null) continue;
-            int gameWin = MoneyConfig.money.getInt ( "money-rewards.game-win" );
+            int gameWin = MoneyConfig.money.getInt("money-rewards.game-win");
             if (gameWin > 0) {
-                BedWars.getEconomy ().giveMoney ( player, gameWin );
-                player.sendMessage ( Language.getMsg ( player, Messages.MONEY_REWARD_WIN ).replace ( "{money}", String.valueOf ( gameWin ) ) );
+                BedWars.getEconomy().giveMoney(player, gameWin);
+                player.sendMessage(Language.getMsg(player, Messages.MONEY_REWARD_WIN).replace("{money}", String.valueOf(gameWin)));
             }
             ITeam bwt = e.getArena().getExTeam(player.getUniqueId());
+            IArena arena = e.getArena();
             if (bwt != null) {
-                //noinspection deprecation
-                if (bwt.getMembersCache().size() > 1) {
-                    int teamMate = MoneyConfig.money.getInt ( "money-rewards.per-teammate" );
+                if (arena.getMaxInTeam() > 1) {
+                    int teamMate = MoneyConfig.money.getInt ("money-rewards.per-teammate");
                     if (teamMate > 0) {
-                        BedWars.getEconomy ().giveMoney ( player, teamMate );
-                        player.sendMessage ( Language.getMsg ( player, Messages.MONEY_REWARD_PER_TEAMMATE ).replace ( "{money}", String.valueOf ( teamMate ) ) );
+                        BedWars.getEconomy().giveMoney(player, teamMate);
+                        player.sendMessage(Language.getMsg(player, Messages.MONEY_REWARD_PER_TEAMMATE).replace("{money}", String.valueOf(teamMate)));
                     }
                 }
             }
         }
-        for (UUID p : e.getLosers ()) {
-            Player player = Bukkit.getPlayer ( p );
+        for (UUID p : e.getLosers()) {
+            Player player = Bukkit.getPlayer(p);
             if (player == null) continue;
             ITeam bwt = e.getArena().getExTeam(player.getUniqueId());
+            IArena arena = e.getArena();
             if (bwt != null) {
-                //noinspection deprecation
-                if (bwt.getMembersCache().size() > 1) {
-                    int teamMate = MoneyConfig.money.getInt ( "money-rewards.per-teammate" );
+                if (arena.getMaxInTeam() > 1) {
+                    int teamMate = MoneyConfig.money.getInt("money-rewards.per-teammate");
                     if (teamMate > 0) {
-                        BedWars.getEconomy ().giveMoney ( player, teamMate );
-                        player.sendMessage ( Language.getMsg ( player, Messages.MONEY_REWARD_PER_TEAMMATE ).replace ( "{money}", String.valueOf ( teamMate ) ) );
+                        BedWars.getEconomy().giveMoney(player, teamMate);
+                        player.sendMessage(Language.getMsg(player, Messages.MONEY_REWARD_PER_TEAMMATE).replace("{money}", String.valueOf(teamMate)));
                     }
                 }
             }
@@ -66,10 +67,10 @@ public class MoneyListeners implements Listener {
     public void onBreakBed(PlayerBedBreakEvent e) {
         Player player = e.getPlayer ();
         if (player == null) return;
-        int bedDestroy = MoneyConfig.money.getInt ( "money-rewards.bed-destroyed" );
+        int bedDestroy = MoneyConfig.money.getInt("money-rewards.bed-destroyed");
         if (bedDestroy > 0) {
-            BedWars.getEconomy ().giveMoney ( player, bedDestroy );
-            player.sendMessage ( Language.getMsg ( player, Messages.MONEY_REWARD_BED_DESTROYED ).replace ( "{money}", String.valueOf ( bedDestroy ) ) );
+            BedWars.getEconomy().giveMoney(player, bedDestroy);
+            player.sendMessage(Language.getMsg( player, Messages.MONEY_REWARD_BED_DESTROYED).replace("{money}", String.valueOf(bedDestroy)));
         }
     }
 
@@ -78,20 +79,20 @@ public class MoneyListeners implements Listener {
      */
     @EventHandler
     public void onKill(PlayerKillEvent e) {
-        Player player = e.getKiller ();
-        Player victim = e.getVictim ();
+        Player player = e.getKiller();
+        Player victim = e.getVictim();
         if (player == null || victim.equals(player)) return;
-        int finalKill = MoneyConfig.money.getInt ( "money-rewards.final-kill" );
-        int regularKill = MoneyConfig.money.getInt ( "money-rewards.regular-kill" );
-        if (e.getCause ().isFinalKill ()) {
+        int finalKill = MoneyConfig.money.getInt("money-rewards.final-kill");
+        int regularKill = MoneyConfig.money.getInt("money-rewards.regular-kill");
+        if (e.getCause().isFinalKill()) {
             if (finalKill > 0) {
-                BedWars.getEconomy ().giveMoney ( player, finalKill );
-                player.sendMessage ( Language.getMsg ( player, Messages.MONEY_REWARD_FINAL_KILL ).replace ( "{money}", String.valueOf ( finalKill ) ) );
+                BedWars.getEconomy().giveMoney(player, finalKill);
+                player.sendMessage(Language.getMsg(player, Messages.MONEY_REWARD_FINAL_KILL).replace ("{money}", String.valueOf(finalKill)));
             }
         } else {
             if (regularKill > 0) {
-                BedWars.getEconomy ().giveMoney ( player, regularKill );
-                player.sendMessage ( Language.getMsg ( player, Messages.MONEY_REWARD_REGULAR_KILL ).replace ( "{money}", String.valueOf ( regularKill ) ) );
+                BedWars.getEconomy().giveMoney(player, regularKill);
+                player.sendMessage(Language.getMsg (player, Messages.MONEY_REWARD_REGULAR_KILL).replace("{money}", String.valueOf(regularKill)));
             }
         }
     }
