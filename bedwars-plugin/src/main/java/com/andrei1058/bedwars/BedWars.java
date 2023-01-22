382817f64b1712a74c5f7078a4317a6759362126
Andrei Dascălu <andrew.dascalu@gmail.com>
2023-01-22 16:41:30 +0100

    fix spigot support #642 (#643)

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/BedWars.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/BedWars.java
@@ -156,7 +156,6 @@ public class BedWars extends JavaPlugin {
             isPaper = true;
         } catch (ClassNotFoundException e) {
             isPaper = false;
-            throw new RuntimeException(e);
         }
 
         plugin = this;
