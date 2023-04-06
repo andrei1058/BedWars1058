3a6adfc57e0be20528fcf4e6bb14f73ccc29d120
MrCeasar <tomkeuper@live.nl>
2023-04-06 21:48:54 +0200

    Revert: Empty line removal

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/upgrades/menu/MenuTrapSlot.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/upgrades/menu/MenuTrapSlot.java
@@ -32,6 +32,7 @@ import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
