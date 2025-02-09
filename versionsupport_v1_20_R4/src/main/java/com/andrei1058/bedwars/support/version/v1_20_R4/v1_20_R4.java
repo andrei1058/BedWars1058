cb72663e8b73196797600eb15649e6d04afa72a5
andrei1058 <andrei7dascalu@gmail.com>
2025-02-09 12:38:00 +0100

    fix #947 on 1.20.4

--- versionsupport_v1_20_R4/src/main/java/com/andrei1058/bedwars/support/version/v1_20_R4/v1_20_R4.java
+++ versionsupport_v1_20_R4/src/main/java/com/andrei1058/bedwars/support/version/v1_20_R4/v1_20_R4.java
@@ -1,12 +1,28 @@
 package com.andrei1058.bedwars.support.version.v1_20_R4;
 
 import com.andrei1058.bedwars.support.version.v1_20_R3.v1_20_R3;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.server.network.PlayerConnection;
+import org.bukkit.craftbukkit.v1_20_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
 public class v1_20_R4 extends v1_20_R3 {
 
     public v1_20_R4(Plugin plugin, String name) {
         super(plugin, name);
+    }
+
+    private void sendPacket(Player player, Packet<?> packet) {
+        ((CraftPlayer) player).getHandle().c.b(packet);
+    }
+
+    private void sendPackets(Player player, Packet<?> @NotNull ... packets) {
+        PlayerConnection connection = ((CraftPlayer) player).getHandle().c;
+        for (Packet<?> p : packets) {
+            connection.b(p);
+        }
     }
 }
