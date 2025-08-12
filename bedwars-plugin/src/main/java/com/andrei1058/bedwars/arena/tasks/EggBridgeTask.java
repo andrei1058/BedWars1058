83d65b386700c33e16017b5a54f4c0bd95999ac1
Computerwhz <samuell@sdcinteractive.co.uk>
2025-08-12 15:15:30 +0100

    Update EggBridgeTask.java

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/arena/tasks/EggBridgeTask.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/arena/tasks/EggBridgeTask.java
@@ -80,8 +80,8 @@ public class EggBridgeTask implements Runnable {
 
         if (getProjectile().isDead()
                 || !arena.isPlayer(getPlayer())
-                || getPlayer().getLocation().distance(getProjectile().getLocation()) > 27
-                || getPlayer().getLocation().getY() - getProjectile().getLocation().getY() > 9) {
+                || getPlayer().getLocation().distance(getProjectile().getLocation()) > 50
+                || getPlayer().getLocation().getY() - getProjectile().getLocation().getY() > 20) {
             EggBridge.removeEgg(projectile);
             return;
         }
